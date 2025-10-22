8f02bb87b3344c8c761afb2a8d377b9699779d39
yigitdede <yigitdedee28@gmail.com>
2025-10-22 20:06:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import os, sys
+os.chdir('/content/ai-knowledge-assistant')
+sys.path.append('/content/ai-knowledge-assistant')
+
+from src import data_loader, model, train
+
+# 1. Veriyi yükle
+data = data_loader.load_data("data/raw")
+
+# 2. Modeli oluştur ve eğit
+rag_model = model.MyModel()
+train.train(rag_model, data)
+
+# 3. Kullanıcı sorgusu
+query = "Yapay zeka nedir?"
+results = rag_model.retrieve(query)
+
+print(f"🔎 Sorgu: {query}")
+print("💡 En yakın bilgi:", results[0])
 
